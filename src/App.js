793c2c4f0ccff97ae03568d661a2440f2589d971
jfouriex <jfouriex@gmail.com>
2019-05-31 10:56:11 +0200

    Erwee

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Jou ma is <code>the center of</code> THE UNIVERSE
+         Hello <code>the center of</code> THE UNIVERSE
         </p>
         <a
           className="App-link"
